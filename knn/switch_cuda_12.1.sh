e8400a4f7acb61b7bd0602df053e9fca71019362
jing <moptisan@qq.com>
2025-07-02 15:33:06 +0800

    cuda 版本切换脚本

---
+++ knn/switch_cuda_12.1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+echo "Switched to CUDA 12.1"
